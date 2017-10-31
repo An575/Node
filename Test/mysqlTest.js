5d0507bcf2e626960c797c9ee8e01ece7fdda8e4
An <tianzhizi575@163.com>
2017-10-31 10:42:21 +0800

    webStorm Start

--- Test/mysqlTest.js
+++ Test/mysqlTest.js
@@ -1,4 +1,5 @@
-/*浦东信息港免费1个月海外mysql临时数据库，需审核1日*/
+/*浦东信息港免费1个月海外mysql临时数据库，需审核1日-无法连接
+* use localhost mysql*/
 var mysql  = require('mysql');  
  
 var connection = mysql.createConnection({     
@@ -12,18 +13,17 @@ var connection = mysql.createConnection({
 connection.connect();
  
 // //1.查
-// var  sql = 'SELECT * FROM websites';
-
-// connection.query(sql,function (err, result) {
-//         if(err){
-//           console.log('[SELECT ERROR] - ',err.message);
-//           return;
-//         }
+var  sql = 'SELECT * FROM websites';
+connection.query(sql,function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
  
-//        console.log('--------------------------SELECT----------------------------');
-//        console.log(result);
-//        console.log('------------------------------------------------------------\n\n');  
-// });
+       console.log('--------------------------SELECT----------------------------');
+       console.log(result);
+       console.log('------------------------------------------------------------\n\n');
+});
 
 //2.增
 // var  addSql = 'INSERT INTO websites(Id,name,url,alexa,country) VALUES(0,?,?,?,?)';
@@ -32,38 +32,37 @@ connection.connect();
 //         if(err){
 //          console.log('[INSERT ERROR] - ',err.message);
 //          return;
-//         }        
- 
+//         }
+//
 //        console.log('--------------------------INSERT----------------------------');
-//        //console.log('INSERT ID:',result.insertId);        
-//        console.log('INSERT ID:',result);        
-//        console.log('-----------------------------------------------------------------\n\n');  
+//        //console.log('INSERT ID:',result.insertId);
+//        console.log('INSERT ID:',result);
+//        console.log('-----------------------------------------------------------------\n\n');
 // });
 
-//3.更
-var modSql = 'UPDATE websites SET name = ?,url = ? WHERE Id = ?';
-var modSqlParams = ['菜鸟移动站', 'https://m.runoob.com',6];
-//改
-connection.query(modSql,modSqlParams,function (err, result) {
-   if(err){
-         console.log('[UPDATE ERROR] - ',err.message);
-         return;
-   }        
-  console.log('--------------------------UPDATE----------------------------');
-  console.log('UPDATE affectedRows',result.affectedRows);
-  console.log('-----------------------------------------------------------------\n\n');
-});
+//3.更改
+// var modSql = 'UPDATE websites SET name = ?,url = ? WHERE Id = ?';
+// var modSqlParams = ['菜鸟移动站', 'https://m.runoob.com',6];
+// connection.query(modSql,modSqlParams,function (err, result) {
+//    if(err){
+//          console.log('[UPDATE ERROR] - ',err.message);
+//          return;
+//    }
+//   console.log('--------------------------UPDATE----------------------------');
+//   console.log('UPDATE affectedRows',result.affectedRows);
+//   console.log('-----------------------------------------------------------------\n\n');
+// });
 
 //4.删
-var delSql = 'DELETE FROM websites where id=6';
-connection.query(delSql,function (err, result) {
-        if(err){
-          console.log('[DELETE ERROR] - ',err.message);
-          return;
-        }        
- 
-       console.log('--------------------------DELETE----------------------------');
-       console.log('DELETE affectedRows',result.affectedRows);
-       console.log('-----------------------------------------------------------------\n\n');  
-});
+// var delSql = 'DELETE FROM websites where id=6';
+// connection.query(delSql,function (err, result) {
+//         if(err){
+//           console.log('[DELETE ERROR] - ',err.message);
+//           return;
+//         }
+//
+//        console.log('--------------------------DELETE----------------------------');
+//        console.log('DELETE affectedRows',result.affectedRows);
+//        console.log('-----------------------------------------------------------------\n\n');
+// });
 connection.end();
